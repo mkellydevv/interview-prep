38cbfa67b1752f4aa7e256eea25c38afa478d04d
mk <m.kelly.devv@gmail.com>
2021-02-02 18:50:51 -0500

    Solved Feb 2 LC challenge

---
+++ leetcode/feb-challenge/2. Trim a Binary Search Tree.js
@@ -0,0 +1,53 @@
+// 2. Trim a Binary Search Tree
+
+function trimBST(root, low, high) {
+    // Trim low
+    let ptr = root;
+    let par = root;
+    while (ptr) {
+        if (ptr.val === low) {
+            ptr.left = null;
+            break;
+        }
+        else if (ptr.val > low) {
+            par = ptr;
+            ptr = ptr.left;
+        }
+        else if (ptr.val < low) {
+            if (ptr === root) {
+                root = ptr.right;
+                ptr = root;
+            }
+            else {
+                par.left = ptr.right;
+                ptr = ptr.right;
+            }
+        }
+    }
+
+    // Trim high
+    par = root;
+    ptr = root;
+    while (ptr) {
+        if (ptr.val === high) {
+            ptr.right = null;
+            break;
+        }
+        else if (ptr.val < high) {
+            par = ptr;
+            ptr = ptr.right;
+        }
+        else if (ptr.val > high) {
+            if (ptr === root) {
+                root = ptr.left;
+                ptr = root;
+            }
+            else {
+                par.right = ptr.left;
+                ptr = ptr.left;
+            }
+        }
+    }
+
+    return root;
+}
