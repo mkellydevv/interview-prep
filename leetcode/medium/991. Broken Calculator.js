7429fac905463423ea0c45caf3e64423e6f4ce7f
mk <m.kelly.devv@gmail.com>
2021-02-22 00:49:31 -0500

    finished Feb 21 - LC991

--- leetcode/medium/991. Broken Calculator.js
+++ leetcode/medium/991. Broken Calculator.js
@@ -1,37 +1,63 @@
+// 991. Broken Calculator
+// #medium #greedy
+// Time    : O(log n)
+// Space   : O(1)
+// Runtime : 72ms,   beats 96.67%
+// Memory  : 38.3mb, beats 83.33%
+// The greedy algorithm starts at target and divides by 2 when even, and adds 1
+// when its odd.  It has to subtract by 1 num - target times when num > tar
+
+// greedy, good
 function brokenCalc(num, tar) {
+    let count = 0;
+    while (num < tar) {
+        if (tar & 1) {
+            tar = ++tar / 2;
+            count += 2;
+        }
+        else {
+            tar /= 2;
+            count++;
+        }
+    }
+    return count + num - tar;
+}
+
+// bfs, queue, bad
+function brokenCalc2(num, tar) {
     if (num > tar)
         return num - tar;
 
     const set = new Set([num]);
-    const stack = [num, null];
+    const queue = [num, null];
     let step = 0;
-    let count = 0;
-    while (num !== tar && count < 10) {
-        let curr = stack.pop();
-        console.log('curr', curr)
+
+    while (true) {
+        const curr = queue.shift();
 
         if (curr === tar)
             return step;
         else if (curr === null) {
             step++;
-            curr = stack.pop();
-            stack.push(null);
+            queue.push(null);
         }
-
-        if (!set.has(curr * 2)) {
-            set.add(curr * 2);
-            stack.push(curr * 2);
+        else {
+            const dbl = curr * 2;
+            const mns = curr - 1;
+            if (!set.has(dbl)) {
+                if (dbl === tar)
+                    return step + 1;
+                set.add(dbl);
+                queue.push(dbl);
+            }
+            if (!set.has(mns)) {
+                if (mns === tar)
+                    return step + 1;
+                set.add(mns);
+                queue.push(mns);
+            }
         }
-        if (!set.has(curr - 1)) {
-            set.add(curr - 1);
-            stack.push(curr - 1);
-        }
-        count++;
-        console.log(stack, set)
-
     }
-    console.log('step', step)
-    return step;
 }
 
-brokenCalc(3, 10);
+console.log(brokenCalc(1, 1000000000))
