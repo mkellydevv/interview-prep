68519771aa0385fef1c334d3f4b06c4b03fea21d
mk <m.kelly.devv@gmail.com>
2021-02-28 15:42:13 -0500

    Refactored LC29 a bit

--- leetcode/medium/29. Divide Two Integers.js
+++ leetcode/medium/29. Divide Two Integers.js
@@ -2,35 +2,41 @@
 // #medium #bitmanip
 // Time    : O(log n)
 // Space   : O(1)
-// Runtime : 100ms,  beats 73.17%
-// Memory  : 39.9mb, beats 90.83%
+// Runtime : 84ms,  beats 98.81%
+// Memory  : 39.7mb, beats 95.76%
 // The algorithm bitwise shifts the divisor to the left as many times as it
 // can while remaining under the dividend.  It subtracts the divisor at this
-// point from the dividend and repeats the process until finished.
+// point from the dividend and repeats the process until finished. If the
+// dividend is less than the divisor but greater than 0, it will be dropped
+// at this point.
 
 function divide(dividend, divisor) {
     if (dividend === -2147483648 && divisor === -1)
         return 2147483647;
-    let quo = 0;
-    let a = Math.abs(dividend);
-    let b = Math.abs(divisor);
+
+    let quotient = 0;
     let sign = -1;
     if (dividend < 0 === divisor < 0)
         sign = 1;
+    dividend = Math.abs(dividend);
+    divisor = Math.abs(divisor);
 
-    if (a === b)
+
+    if (dividend === divisor)
         return sign;
 
-    while (a >= b) {
-        let div = b;
+    while (dividend >= divisor) {
+        let subtrahend = divisor;
         let i = 0;
-        while (div > 0 && div <= a)
-            div = b << ++i;
-        a -= b << i - 1;
-        quo += 1 << i - 1;
+        while (subtrahend > 0 && subtrahend <= dividend){
+            subtrahend <<= 1;
+            i++;
+        }
+        dividend -= subtrahend >>> 1;
+        quotient += 1 << i - 1;
     }
 
     if (sign > 0)
-        return quo;
-    return -quo;
+        return quotient;
+    return -quotient;
 }
