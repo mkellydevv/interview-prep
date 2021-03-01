33244cc592171af1b5421f0efe61cf8fa1592706
mk <m.kelly.devv@gmail.com>
2021-03-01 14:12:23 -0500

    Updated lc575

--- leetcode/easy/575. Distribute Candies.js
+++ leetcode/easy/575. Distribute Candies.js
@@ -2,14 +2,14 @@
 // #easy #set
 // Time    : O(n)
 // Space   : O(n)
-// Runtime : 80ms,   beats 90.21%
-// Memory  : 40.9mb, beats 24.02%
+// Runtime : 124ms,  beats 95.40%
+// Memory  : 52.1mb, beats 82.85%
 // The algorithm iterates over the array and keeps track of what it has seen
 // before.  It returns the min of types / 2 and the number of types.
 
-function distributeCandies(types) {
+function distributeCandies(arr) {
     const set = new Set();
-    for (let i = 0; i < types.length; i++)
-        set.add(types[i]);
-    return Math.min(types.length >> 1, set.size);
+    for (let el of arr)
+        set.add(el);
+    return Math.min(arr.length >> 1, set.size);
 }
